3c5acb87d47d6e32fe1e264a809bc08d735b4b17
Artem Rozborskyi <artemrozbor@gmail.com>
2026-08-17 19:00:08 +0300

    Fix UserManagerWrapper class

--- BookingManagerWeb/BookingManagerWeb.Infrastructure/Identity/Wrappers/IUserManagerWrapper.cs
+++ BookingManagerWeb/BookingManagerWeb.Infrastructure/Identity/Wrappers/IUserManagerWrapper.cs
@@ -2,10 +2,10 @@ using Microsoft.AspNetCore.Identity;
 
 namespace BookingManagerWeb.Infrastructure.Identity.Wrappers;
 
-public abstract class IUserManagerWrapper
+public interface IUserManagerWrapper
 {
-    public abstract Task<IdentityResult> CreateAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default);
-    public abstract Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role, CancellationToken cancellationToken = default);
-    public abstract Task<ApplicationUser?> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken = default);
-    public abstract Task<bool> CheckPasswordAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default);
+    public Task<IdentityResult> CreateAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default);
+    public Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role, CancellationToken cancellationToken = default);
+    public Task<ApplicationUser?> FindByEmailAsync(string normalizedEmail, CancellationToken cancellationToken = default);
+    public Task<bool> CheckPasswordAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default);
 }
--- BookingManagerWeb/BookingManagerWeb.Infrastructure/Identity/Wrappers/UserManagerWrapper.cs
+++ BookingManagerWeb/BookingManagerWeb.Infrastructure/Identity/Wrappers/UserManagerWrapper.cs
@@ -4,23 +4,23 @@ namespace BookingManagerWeb.Infrastructure.Identity.Wrappers;
 
 public class UserManagerWrapper (UserManager<ApplicationUser> userManager) : IUserManagerWrapper
 {
-    public override async Task<IdentityResult> CreateAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default)
+    public async Task<IdentityResult> CreateAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default)
     {
         return await userManager.CreateAsync(user, password);
     }
 
-    public override async Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role, CancellationToken cancellationToken = default)
+    public async Task<IdentityResult> AddToRoleAsync(ApplicationUser user, string role, CancellationToken cancellationToken = default)
     {
         return await userManager.AddToRoleAsync(user, role);
     }
 
-    public override async Task<ApplicationUser?> FindByEmailAsync(string normalizedEmail,
+    public async Task<ApplicationUser?> FindByEmailAsync(string normalizedEmail,
         CancellationToken cancellationToken = default)
     {
         return await userManager.FindByEmailAsync(normalizedEmail);
     }
 
-    public override async Task<bool> CheckPasswordAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default)
+    public async Task<bool> CheckPasswordAsync(ApplicationUser user, string password, CancellationToken cancellationToken = default)
     {
         return await userManager.CheckPasswordAsync(user, password);
     }
